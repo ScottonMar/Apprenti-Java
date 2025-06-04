43694ded997fda138543ee095751c45bdaf71d84
ScottonMar <ms.marquitaevette@gmail.com>
2025-06-04 11:26:26 -0400

    Create TheAssignmentOperator.java

---
+++ CodeAlongOperators/src/TheAssignmentOperator.java
@@ -0,0 +1,13 @@
+public class TheAssignmentOperator {
+    public static void main(String[] args) {
+        System.out.println("The Assignment Operator");
+
+        // Declare and initialize x
+        System.out.println("int x = 0");
+        int x = 0;
+
+        //Display the value of x
+        System.out.println("The is the value of x");
+        System.out.println(x);
+    }
+}
